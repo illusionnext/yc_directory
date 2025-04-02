20f49ec5dad9bbb8aa1a74dde1c81d59710acf6f
Farid <fargan@st.amu.edu.pl>
2025-04-03 00:01:22 +0200

    ➕ package.json@latest

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,4 @@
 const Home = () => {
-  return (
-    <h1 className="grid w-full h-full place-content-center place-items-center">
-      Home (root)
-    </h1>
-  );
+  return <h1 className="heading">Home (root)</h1>;
 };
 export default Home;
